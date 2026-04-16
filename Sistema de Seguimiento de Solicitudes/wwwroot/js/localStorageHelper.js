b4ddabe1b2165aec75ead390c3a31de38d29f403
arael558 <discordarael@gmail.com>
2026-04-16 06:38:45 -0700

    prueba

--- Sistema de Seguimiento de Solicitudes/wwwroot/js/localStorageHelper.js
+++
@@ -1,6 +0,0 @@
-﻿window.localStorageHelper = {
-    setItem: (key, value) => localStorage.setItem(key, value),
-    getItem: (key) => localStorage.getItem(key),
-    removeItem: (key) => localStorage.removeItem(key),
-    clear: () => localStorage.clear()
-};
--- SolicitudesAPI/Models/DiaInhabilManual.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SolicitudesAPI.Models
-{
-    public partial class DiaInhabilManual
-    {
-        public int Id { get; set; }
-        public DateTime Fecha { get; set; }
-    }
-}
